19d4bd36f7a0c915eb37b56beb7e2608efbe8b49
Antonio Maiorano <amaiorano@gmail.com>
2011-01-12 23:55:28 -0500

    Graphics Changes: - Modified build_graphics.bat to build and output only the files required for the game - Adding dungeon and cave tile bmps - Added tiles_palette.grit to export the palette, disabled palette export from the 16x16 and 32x32 grit files.
    [git-p4: depot-paths = "//depot/NDS/ZeldaDS/": change = 228]

--- Assets/Graphics/build_graphics.bat
+++ Assets/Graphics/build_graphics.bat
@@ -1,12 +1,39 @@
 setlocal
+pushd %~dp0
+
 set GRIT=%NDSGAMEROOT%\Tools\grit\grit.exe
+set TGT_PATH=output
+set SRC_PATH=..
+
+rem Delete and recreate output folder
+rm -rf %TGT_PATH%
+md %TGT_PATH%
 
 rem No easy way to tell grit to output to a specified folder, so we go into the target
 rem folder, and run the commands relative to this folder
-md output
-cd output
+cd %TGT_PATH%
 
 rem TODO: Consider renaming 16x16 files to *_16x16.bmp and simplify this command 
-ls ../*.bmp | grep "_32x32.bmp" -v | xargs -ixxx %GRIT% xxx -ff ../tiles16x16.grit
+rem ls ../*.bmp | grep "_32x32.bmp" -v | xargs -ixxx %GRIT% xxx -ff ../tiles16x16.grit
+rem ls ../*_32x32.bmp | xargs -ixxx %GRIT% xxx -ff ../tiles32x32.grit
 
-ls ../*_32x32.bmp | xargs -ixxx %GRIT% xxx -ff ../tiles32x32.grit
+rem Export palette data (.pal)
+%GRIT% %SRC_PATH%/characters.bmp -ff %SRC_PATH%/tiles_palette.grit -o sprite_palette
+%GRIT% %SRC_PATH%/overworld_bg.bmp -ff %SRC_PATH%/tiles_palette.grit -o tile_palette
+
+rem Export image data (.img)
+%GRIT% %SRC_PATH%/items.bmp -ff %SRC_PATH%/tiles16x16.grit
+
+%GRIT% %SRC_PATH%/characters.bmp -ff %SRC_PATH%/tiles16x16.grit
+%GRIT% %SRC_PATH%/characters_32x32.bmp -ff %SRC_PATH%/tiles32x32.grit
+
+%GRIT% %SRC_PATH%/overworld_bg.bmp -ff %SRC_PATH%/tiles16x16.grit
+%GRIT% %SRC_PATH%/overworld_fg.bmp -ff %SRC_PATH%/tiles16x16.grit
+
+%GRIT% %SRC_PATH%/dungeon_bg.bmp -ff %SRC_PATH%/tiles16x16.grit
+%GRIT% %SRC_PATH%/dungeon_fg.bmp -ff %SRC_PATH%/tiles16x16.grit
+
+rem Now remove the bin extension
+rename *.bin *.
+
+popd
