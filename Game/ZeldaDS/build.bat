e420b4f92bb5bb1a2104c8668a25de528b9e0c0b
Antonio Maiorano <amaiorano@gmail.com>
2014-05-01 00:17:33 -0400

    Fix makefile build errors not causing Visual Studio build to fail
    Error List window is also correctly populated with errors

--- Game/ZeldaDS/build.bat
+++ Game/ZeldaDS/build.bat
@@ -13,8 +13,12 @@ echo Invalid input param!
 goto :END
 
 :BUILD
-make CONFIG=%CONFIG%
-goto :POSTBUILD
+rem Pipe output of stderr and stdout to sed to format errors for Visual Studio. Unfortunately sed overwrites the return
+rem status of make, so we tee the output to a temp file and grep it to see if any errors occured.
+make CONFIG=%CONFIG% 2>&1 |tee %TEMP%\zdsbuildoutput.txt | sed -e 's@\(.[a-zA-Z]\+\):\([0-9]\+\):\([0-9]\+\):@\1(\2,\3):@g'
+grep "error:" "%TEMP%\zdsbuildoutput.txt"
+if not %errorlevel%==0 goto :POSTBUILD
+goto :END
 
 :CLEAN
 make clean CONFIG=%CONFIG%
@@ -22,8 +26,7 @@ goto :END
 
 :REBUILD
 make clean CONFIG=%CONFIG%
-make CONFIG=%CONFIG%
-goto :POSTBUILD
+goto :BUILD
 
 :POSTBUILD
 make printsize CONFIG=%CONFIG%
